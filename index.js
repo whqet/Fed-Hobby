2d91d9967d0b4bc991ad2eea765fad059a03ed98
whqet <whqet@qq.com>
2016-07-15 10:17:48 +0800

    update readme.md

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ var markdown = require('raw!./README.md');
 var data = parse(markdown);
 
 var m = markmap('svg#mindmap', transform(data), {
-  preset: 'colorful2',
+  preset: 'colorful',
+  nodeWidth: 60,
+	nodeHeight: 20,
   textIndent: 40,
   autoFit: false,
-  zoomTranslate: [100, 0],
+  zoomTranslate: [50, 0],
   zoomScale: 1,
   depth: 3,
-  scale: [0.5,1]
+  scale: [1,2]
 });
 
 document.getElementById('J_expand').onclick = function() {
